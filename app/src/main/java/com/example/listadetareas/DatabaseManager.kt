d393aed1ed2c11b57874c05648fc6819ae46f430
Bee1002 <alvaroslim@gmail.com>
2025-04-30 19:17:33 +0200

    add DatabaseManager

--- app/src/main/java/com/example/listadetareas/DatabaseManager.kt
+++ app/src/main/java/com/example/listadetareas/DatabaseManager.kt
@@ -9,13 +9,25 @@ class DatabaseManager (context: Context) : SQLiteOpenHelper (context, DATABASE_N
     companion object {
         const val  DATABASE_VERSION = 1
         const val  DATABASE_NAME = "ToDoList.db"
+
+        private const val SQL_CREATE_CATEGORY =
+            "CREATE TABLE ${Category.TABLE_NAME} (" +
+                    "${Category.COLUMN_NAME_ID} INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "${Category.COLUMN_NAME_TITLE} TEXT)"
+
+        private const val SQL_DELETE_CATEGORY = "DROP TABLE IF EXISTS ${Category.TABLE_NAME}"
     }
 
-    override fun onCreate(db: SQLiteDatabase?) {
-        TODO("Not yet implemented")
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(SQL_CREATE_CATEGORY)
     }
 
-    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        TODO("Not yet implemented")
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        onDestroy(db)
+        onCreate(db)
+    }
+
+    private  fun onDestroy (db: SQLiteDatabase) {
+        db.execSQL(SQL_DELETE_CATEGORY)
     }
 }
