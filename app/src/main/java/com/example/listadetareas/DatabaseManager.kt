29f2aebf0b8b9d53b4849c1ed16927918436843a
Bee1002 <alvaroslim@gmail.com>
2025-04-30 16:35:05 +0200

    add DatabaseManager

---
+++ app/src/main/java/com/example/listadetareas/DatabaseManager.kt
@@ -0,0 +1,21 @@
+package com.example.listadetareas
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseManager (context: Context) : SQLiteOpenHelper (context, DATABASE_NAME, null,DATABASE_VERSION) {
+
+    companion object {
+        const val  DATABASE_VERSION = 1
+        const val  DATABASE_NAME = "ToDoList.db"
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+}
